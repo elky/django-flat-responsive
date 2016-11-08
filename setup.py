9c6fa9f4b5318f580d8bf9e70a03f89c17fce529
elky <mansellfan@gmail.com>
2016-11-08 13:05:18 +1100

    Package preparations

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-flat-responsive',
+    packages=find_packages(),
+    version=__import__('flat_responsive').__version__,
+    author='elky',
+    author_email='mail@elky.me',
+    description=('An extension for Django admin that makes interface good for mobile and tablets.'),
+    license='BSD',
+    url='https://github.com/elky/django-flat-responsive',
+    keywords=['django', 'admin', 'responsive', 'mobile', 'interface'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
